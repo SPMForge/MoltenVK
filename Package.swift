d84a625d4df8798c526a53337417b053c9ca0b14
rbbtsn0w <hamiltonsnow@gmail.com>
2026-04-23 15:54:45 +0800

    fix: update MoltenVK binary target URL and checksum to version 1.4.1-alpha.1

--- Package.swift
+++ Package.swift
@@ -20,8 +20,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MoltenVK",
-            url: "https://github.com/SPMForge/MoltenVK/releases/download/1.4.1-alpha.8/MoltenVK-1.4.1-alpha.8.xcframework.zip",
-            checksum: "a0d9188b33e1210e90751c89d8baa893169ffd377212bec70b55454d653a8c5c"
+            url: "https://github.com/SPMForge/MoltenVK/releases/download/1.4.1-alpha.1/MoltenVK-1.4.1-alpha.1.xcframework.zip",
+            checksum: "469e0a7e2496d653a526f0b4ce2aaeb37ab22ff3e4f19e65fbdde5aa5a2d7d03"
         ),
     ]
 )
